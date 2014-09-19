93acfc31c7e099bdb31933c3269e26f7a7a0d1a8
Kurt Mitchell <kmitchell2300@gmail.com>
2014-09-19 13:14:54 -0400

    Initial Commit of Infinyte Bingo Engine Base
    The Ball class and a main testing static area

---
+++ InfinyteBingo/Ball.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InfinyteBingo
+{
+    class Ball 
+    {
+        // Properties
+        
+        // Private Member Variables
+        private char _Letter;               // The Letter Of The Ball (B,I,N,G,O) 
+        private int _Number;                // The Number on the Ball (1-75)
+        private Color _BallColor;           // The Color of The Ball (Blue, Purple, Red, Green, Orange)
+
+
+        // Constructors
+        
+        // Default Constructor
+        public Ball()
+        {
+            // Setting all of the ball's default properties to invalid values.
+            _Letter = 'Z';
+            _Number = 0;
+            _BallColor = Color.Transparent;
+        }
+        
+        // Overloaded Constructor 
+        // @params : (char BallsLetter, int BallsNumber, Color BallsColor)
+        public Ball(char l, int n, Color c)
+        {
+            
+            
+            _Letter = l;
+            _Number = n;
+            _BallColor = c;
+        }
+
+        // Class Methods
+
+        // Is Ball Valid Method
+        public bool isBallValid()
+        {
+            
+            // Test Balls that are B
+            if (GetLetter() == 'B')
+            {
+                if ( (GetNumber() > 0) && (GetNumber() < 16) )
+                {
+                    if (GetColor() == Color.Blue)
+                        return true;
+                }
+            }
+            return false;
+        }
+          
+      
+        // Ball Properties Public Get/Set Methods
+        public char GetLetter()
+        {
+            return _Letter;
+        }
+
+        public int GetNumber()
+        {
+            return _Number;
+        }
+        public Color GetColor()
+        {
+            return _BallColor;
+        }
+
+        public void SetLetter(char l)
+        {
+            _Letter = l;
+        }
+
+        public void SetNumber(int n)
+        {
+            _Number = n;
+        }
+        public void SetColor(Color c)
+        {
+            _BallColor = c;
+        }
+
+        // Setter to Set All of Ball's Properties
+        public void SetAllProperties(char l, int n, Color c)
+        {
+            this.SetNumber(n);
+            this.SetColor(c);
+            this.SetLetter(l);
+        }
+
+        // Debug Methods 
+        
+        // Output Ball's Details to Console
+        public void ShowDetails()
+        {
+            System.Console.WriteLine( this.GetDetailsString() );
+        }
+
+        // Get a String containing All of Ball's Private Propertie's Info (Letter,Number and Color)
+        public String GetDetailsString() 
+        {
+            return (_Letter + " " + _Number +  ", is " + _BallColor + '\n');
+        }
+    
+    }
+}
---
+++ InfinyteBingo/MainAppTestees.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace InfinyteBingo
+{
+    class MainAppTestees 
+    {
+        static void Main(string[] args)
+        {
+            // Main Application Testing Area
+            // This is the only place in this Solution where you can actually create, modify and delete 
+            // Objects(Classes) in Memory.
+
+            String DebugLine = "";      // A Variable to temporarily hold a line of Debugging Output
+
+            // Insert Testing Code After This Message 
+            Display("\n\n\n***** Beginning of Debuggin Output for The Basic Ass Bingo Engine *******\n\n\n");
+
+            // Your Code goes here ()=====>
+            Display("Insert Your ()=========>\'in Around Here!");
+
+            Ball b = new Ball('B', 17, Color.Blue);
+            
+            Display("Ball is valid:" + b.isBallValid() );
+            b.ShowDetails();
+
+            // End of your code - These Lines are to keep the console open till you press enter
+            Display("\n\n\n***** End of Debugging Output! Press Enter to Continue/Quit ***********");
+            DebugLine = System.Console.ReadLine();
+            
+        }
+
+        // Static Helper Method to Display a String to the Console
+        static void Display(String s)
+        {
+            System.Console.Out.WriteLine(s);
+        }
+
+       
+    }
+}
